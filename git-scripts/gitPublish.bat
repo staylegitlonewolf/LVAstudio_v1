152d39d18ed74950866b4c2cf322a86eab53d26f
staylegitlonewolf <staylegitlonewolf@gmail.com>
2025-07-17 11:39:18 -0400

    Auto update

--- git-scripts/gitPublish.bat
+++ git-scripts/gitPublish.bat
@@ -1,7 +1,6 @@
 @echo off
 echo Saving ALL files and folders in the project...
-set /p msg="Enter commit message: "
 git add .
-git commit -m "%msg%"
+git commit -m "Auto update"
 git push
 pause 
--- git-scripts/gitSave.bat
+++ git-scripts/gitSave.bat
@@ -1,6 +1,5 @@
 @echo off
 echo Saving ALL files and folders in the project...
-set /p msg="Enter commit message: "
 git add .
-git commit -m "%msg%"
+git commit -m "Auto update"
 pause 
